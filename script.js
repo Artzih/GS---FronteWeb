5bc14c22bd192cf8e31b298f375def1ddd801ef3
Arthur Kazuo <arthur@ladio.com.br>
2026-06-01 19:47:49 -0300

    adicionando imagemshow

---
+++ script.js
@@ -0,0 +1,25 @@
+const imagens = [
+    "./imgs/kesler1.png",
+    "./imgs/kesler2.png",
+    "./imgs/kesler3.png"
+];
+
+let index = 0;
+
+function mostrarSlide (){
+    document.getElementById('imagem').src = imagens[index];
+};
+function voltarSlide (){
+    index--
+    if (index < 0) {
+        index = imagens.length - 1
+    }
+    mostrarSlide();
+};
+function proximoSlide (){
+    index++
+    if (index >= imagens.length) {
+        index = 0
+    }
+    mostrarSlide();
+}
